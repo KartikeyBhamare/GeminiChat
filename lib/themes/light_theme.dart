b5f55421364c1dec7ca6c2a412d5e43102896195
Kartikey Bhamare <98818952+KartikeyBhamare@users.noreply.github.com>
2024-06-29 18:57:20 +0530

    light theme related data

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightmode = ThemeData(
+    colorScheme: ColorScheme.light(
+  surface: Colors.grey.shade300,
+  primary: Colors.grey.shade500,
+  secondary: Colors.grey.shade800,
+  inversePrimary: Colors.grey.shade900,
+));
